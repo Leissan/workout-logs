c61152917cc5852fe91fb761b8249fe408a503c5
Leissan Khan <leissan.khan@gmail.com>
2023-03-05 09:35:05 -0500

    comp

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Switch, Route } from "react-router-dom";
+import NavBar from "./NavBar";
+import Login from "../pages/Login";
+import NewExercise from "../pages/NewExercise";
+import ExerciseList from "../pages/ExerciseList";
+import NewLog from "../pages/NewLog";
+import LogList from "../pages/LogList";
+import axios from 'axios'
 
 function App() {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    // auto-login
+    axios.get("/me").then((r) => {
+      console.log(r.status)
+      if (r.status === 200) {
+        setUser(r.data)
+      }
+    });
+  }, []);
+
+  if (!user) return <Login onLogin={setUser} />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar user={user} setUser={setUser} />
+      <main>
+        <Switch>
+          <Route path="/new_exercise">
+            <NewExercise user={user} />
+          </Route>
+          <Route path="/exercises">
+            <ExerciseList user={user} />
+          </Route>
+          <Route path="/new_logs">
+            <NewLog user={user} />
+          </Route>
+          <Route path="/history">
+            <LogList user={user} />
+          </Route>
+          <Route path="/">
+            <LogList user={user} />
+          </Route>
+        </Switch>
+      </main>
+    </>
   );
 }
 
